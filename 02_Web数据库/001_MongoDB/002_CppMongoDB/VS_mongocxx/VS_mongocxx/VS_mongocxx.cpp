2bdc5679f8a0e273d4c9bc03a220784a1cb75a7d
Jacob-xyb <949782197@qq.com>
2022-10-14 18:13:36 +0800

    add mongocxx lib

---
+++ 02_Web数据库/001_MongoDB/002_CppMongoDB/VS_mongocxx/VS_mongocxx/VS_mongocxx.cpp
@@ -0,0 +1,62 @@
+﻿// VS_mongocxx.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
+#include <bsoncxx/builder/stream/helpers.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/builder/stream/array.hpp>
+
+using bsoncxx::builder::stream::close_array;
+using bsoncxx::builder::stream::close_document;
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
+using bsoncxx::builder::stream::open_array;
+using bsoncxx::builder::stream::open_document;
+
+int main()
+{
+	mongocxx::instance instance{}; // This should be done only once.
+	mongocxx::client client{ mongocxx::uri{"mongodb://localhost:27017"} };
+	mongocxx::database db = client["test"];
+	mongocxx::collection coll = db["col"];
+
+	auto builder = bsoncxx::builder::stream::document{};
+	bsoncxx::document::value doc_value = builder
+		<< "name" << "MongoDB"
+		<< "type" << "database"
+		<< "count" << 1
+		<< "versions" << bsoncxx::builder::stream::open_array
+		<< "v3.2" << "v3.0" << "v2.6"
+		<< close_array
+		<< "info" << bsoncxx::builder::stream::open_document
+		<< "x" << 203
+		<< "y" << 102
+		<< bsoncxx::builder::stream::close_document
+		<< bsoncxx::builder::stream::finalize;
+	coll.insert_one(doc_value.view());
+
+	mongocxx::cursor cursor = coll.find(bsoncxx::document::view({}));
+	for (auto doc : cursor) {
+		std::cout << bsoncxx::to_json(doc) << "\n";
+	}
+	std::cout << "Hello World!\n";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
